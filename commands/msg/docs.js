eb778329661691fc87231035c3d76ad8139e92e8
SriHari-15 <sriharidev1508@gmail.com>
2021-09-04 09:53:29 +0530

    Docs update

--- commands/msg/docs.js
+++ commands/msg/docs.js
@@ -23,6 +23,60 @@ module.exports = async (message, args) => {
         "When a new version is released, the Popstar launcher will notify you about it. In order to update the menu, delete all the files that you extracted from the ZIP and download the menu again from the website. Updating 2Take1Menu will only replace the menu files; the menu data, such as credentials, settings and other assets, will remain intact."
       );
       break;
+    case "self":
+        message.channel.send("<https://gta.2take1.menu/features/local/player-options>")
+        break;
+    case "veh":
+        message.channel.send("<https://gta.2take1.menu/features/local/vehicle-options>")
+        break;
+    case "model":
+        message.channel.send("<https://gta.2take1.menu/features/local/model-changer>")
+        break;
+    case "outfit":
+        message.channel.send("<https://gta.2take1.menu/features/local/outfits>")
+        break;
+    case "outfitter":
+        message.channel.send("<https://gta.2take1.menu/features/local/outfitter>")
+        break;
+    case "anim":
+        message.channel.send("<https://gta.2take1.menu/features/local/animations>")
+        break;
+    case "ptfx":
+        message.channel.send("<https://gta.2take1.menu/features/local/ptfx>")
+        break;
+    case "tp":
+        message.channel.send("<https://gta.2take1.menu/features/local/teleport>")
+        break;
+    case "aim":
+        message.channel.send("<https://gta.2take1.menu/features/local/aim-assist>")
+        break
+    case "weapon":
+        message.channel.send("<https://gta.2take1.menu/features/local/weapons>")
+        break
+    case "weather":
+        message.channel.send("<https://gta.2take1.menu/features/local/weather-and-time>")
+        break;
+    case "lua":
+        message.channel.send("<https://gta.2take1.menu/features/local/scripts>")
+        break;
+    case "misc":
+        message.channel.send("<https://gta.2take1.menu/features/local/misc>")
+        break;
+    case "settings":
+        message.channel.send("<https://gta.2take1.menu/features/local/settings>")
+        break;
+    case "online":
+        message.channel.send("<https://gta.2take1.menu/features/online/online-players>")
+        break;
+    case "everyone":
+        message.channel.send("<https://gta.2take1.menu/features/online/all-players>")
+        break;
+    case "lobby":
+        message.channel.send("<https://gta.2take1.menu/features/online/lobby>")
+        break;
+    case "spoof":
+        message.channel.send("<https://gta.2take1.menu/features/online/spoofer>")
+        break;
     default:
       break;
   }
